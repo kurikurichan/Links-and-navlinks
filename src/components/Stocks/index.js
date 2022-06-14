2b475dd336ff2d8cd5428e3126dee9d7dae4ea89
Krista Strucke <kstrucke@gmail.com>
2022-06-14 14:22:46 -0700

    finished

--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,7 +1,20 @@
+import { Redirect, useHistory } from "react-router-dom";
+
 function Stocks() {
+  const history = useHistory();
+  const loggedIn = true;
+
+  function handleClick() {
+    window.alert("Sending info to the DB!");
+    history.push("/");
+  }
+
+  if (!loggedIn) return <Redirect to="/not-logged-in" />;
+
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
